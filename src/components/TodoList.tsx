3104b7733de25482c0369ae4c9e33f41e2b7d2da
Murhussain <husseinmurashi@yahoo.com>
2024-09-18 13:29:51 +0200

    Done with adding new todo payload

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,7 +14,7 @@ const TodoList: React.FC = () => {
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-            <ul>
+            <ul className="">
                 {todos.map((todo) => (
                     <li key={todo.id} className="mb-2 p-4 bg-gray-100 rounded shadow">
                         <h2 className="text-lg font-semibold">{todo.todo}</h2>
